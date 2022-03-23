7dde0409fa4e5418c7f2276f5adfea1e907a371e
Vis Rodrigues <visrodrigues@MacBook-Air-de-Vis.local>
2022-03-22 21:01:28 -0300

    adicionado sg

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ resource "aws_instance" "fiap-hmv-ec2" {
   instance_type = var.ec2_instance_type
   key_name      = var.ec2_key_name
 
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+
   tags = {
     Projeto = var.projeto
-    Name    = "${var.ec2_name}-${count.index}"
+    Name    = "${var.ec2_name}"
   }
 }
